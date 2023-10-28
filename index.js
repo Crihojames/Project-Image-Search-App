5c9d5d0175c5fc9b2cb9124fa418e6f19bdaa110
Crihojames <crihojames@gmail.com>
2023-10-28 10:12:32 +0100

    add to finiche js and my projet

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const accesskey = "Njo1jCjHylkI8-zPfZ2fKRemp94-b9Epkc9HdnK-s1E"
 const formEl = document.querySelector("form")
 const searchInputEl = document.getElementById("search-input")
 const searchResulteEl = document.querySelector(".search-results")
-const showMoreButton = document.getElementById("show-more-button")
+const showMoreButtonEl = document.getElementById("show-more-button")
 
 let inputData = "";
 let page = 1;
@@ -11,16 +11,37 @@ let page = 1;
 async function searchImages(){
     inputData = searchInputEl.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesskey}`;
-    console.log(url);
     const response = await fetch(url);
     const data = await response.json();
     if(page === 1){
         searchResulteEl.innerHTML = ""
     }
 
-    const results = data.results
+    const results = data.results;
 
-    console.log(results);
+
+    results.map(result => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a")
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink)
+    searchResulteEl.appendChild(imageWrapper)
+    });
+
+    page++;
+
+    if (page > 1) {
+        showMoreButtonEl.style.display = "block";
+    }
+
 }
 
 formEl.addEventListener("submit", (event) => {
@@ -28,4 +49,7 @@ formEl.addEventListener("submit", (event) => {
     page = 1;
     searchImages();
 } )
-// messi
+
+showMoreButtonEl.addEventListener("click", () => {
+    searchImages();
+})
